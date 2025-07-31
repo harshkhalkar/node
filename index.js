d610c6e437d5f65563e6149c03394a0c38ccd5ae
harshkhalkar <harshkhalkar@gmail.com>
2025-07-31 05:53:21 +0000

    node app

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();//Respond with "hello world" for requests that hit our root 
 
 app.get('/', function (req, res) { 
 
- res.send('changed not '); 
+ res.send('Node App '); 
 
 });//listen to port 3000 by default 
 
